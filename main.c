3c9f78c7aee529a6af67d14043311f0a0f4adbd2
Anders Pitman <tapitman11@gmail.com>
2022-05-24 17:01:04 -0600

    Implement benchmarking

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+// Needed for clock_gettime when compiling for C11, see here:
+// https://stackoverflow.com/a/40515669
+#define _POSIX_C_SOURCE 199309L
+
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
@@ -5,6 +9,23 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdint.h>
+
+
+// Taken from here: https://www.guyrutenberg.com/2007/09/22/profiling-code-using-clock_gettime/
+struct timespec diff(struct timespec start, struct timespec end) {
+	struct timespec temp;
+	if ((end.tv_nsec-start.tv_nsec)<0) {
+		temp.tv_sec = end.tv_sec-start.tv_sec-1;
+		temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+	} else {
+		temp.tv_sec = end.tv_sec-start.tv_sec;
+		temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+	}
+	return temp;
+}
+
 
 void readDirect(char *filePath, size_t size, size_t bufSize, FILE *outFile) {
     FILE *fp = fopen(filePath, "r");
@@ -72,16 +93,21 @@ int main (int argc, char** argv) {
         return 1;
     }
 
-    fprintf(stdout, "Size: %ld\n", st.st_size);
+    //fprintf(stdout, "Size: %ld\n", st.st_size);
 
     FILE *outFile = fopen(outPath, "w");
 
+    struct timespec start;
+    struct timespec end;
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
     if (0 == strcmp(command, "fread")) {
-        fprintf(stdout, "Using fread\n");
+        fprintf(stderr, "Using fread\n");
         readDirect(filePath, st.st_size, bufSize, outFile);
     }
     else if (0 == strcmp(command, "mmap")) {
-        fprintf(stdout, "Using mmap\n");
+        fprintf(stderr, "Using mmap\n");
         readMmap(filePath, st.st_size, bufSize, outFile);
     }
     else {
@@ -90,6 +116,18 @@ int main (int argc, char** argv) {
     }
 
     fclose(outFile);
-   
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    struct timespec elapsed = diff(start, end);
+    double fractionalSeconds = (double)elapsed.tv_nsec / 1000000000.0;
+    double elapsedSeconds = (double)elapsed.tv_sec + fractionalSeconds;
+
+    double gbps = ((double)st.st_size / 1000000000.0) / elapsedSeconds;
+
+    fprintf(stderr, "%lu bytes copied in %.3lf seconds (%.3lf GB/s)\n", st.st_size, elapsedSeconds, gbps);
+    fprintf(stdout, "command\tbufSize\tfileSize\telapsedSeconds\tGB/s\n");
+    fprintf(stdout, "%s\t%d\t%lu\t%lf\t%.3lf\n", command, bufSize, st.st_size, elapsedSeconds, gbps);
+
     return(0);
 }
